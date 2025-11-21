7373e0c48f7be0df434fd525c6b71e0983cb2a92
luimu64 <luimu@luimu.dev>
2025-11-21 15:48:38 +0200

    init

---
+++ main.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <random>
+#include <vector>
+
+const int room_count_min = 40;
+const int room_count_max = 300;
+const int price_min = 80;
+const int price_max = 100;
+
+class Room {
+private:
+  int price_per_night;
+  bool is_booked;
+  int capacity;
+
+public:
+  Room(int capacity, std::mt19937 &gen) : capacity(capacity) {
+    std::uniform_int_distribution<> price_dist(price_min, price_max);
+    price_per_night = price_dist(gen);
+    is_booked = false;
+  }
+
+  void print_room_info() const {
+    std::cout << "Hinta yöltä: " << price_per_night
+              << ", Varattu: " << (is_booked ? "Kyllä" : "Ei") << std::endl;
+  }
+};
+
+inline int make_even(int n) { return n - n % 2; }
+
+std::vector<Room> create_rooms(std::mt19937 &gen) {
+  std::vector<Room> rooms;
+
+  std::uniform_int_distribution<> room_count_dist(room_count_min,
+                                                  room_count_max);
+  int room_count = make_even(room_count_dist(gen));
+
+  for (int i = 0; i < room_count; ++i) {
+    rooms.emplace_back(gen);
+  }
+
+  return rooms;
+}
+
+class Hotel {
+private:
+  std::vector<Room> rooms;
+
+public:
+  Hotel(std::mt19937 &gen) : rooms(create_rooms(gen)) {}
+
+  void print_rooms_info() const {
+    for (const auto &room : rooms) {
+      room.print_room_info();
+    }
+  }
+
+  int get_room_count() const { return rooms.size(); }
+};
+
+int main() {
+  std::random_device rd;
+  std::mt19937 gen(rd());
+
+  Hotel hotel(gen);
+  hotel.print_rooms_info();
+
+  std::cout << hotel.get_room_count() << std::endl;
+
+  return 0;
+}
