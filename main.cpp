1ecd03c4103d8b1b1545f8fe7d8b53be33f65567
luimu64 <luimu@luimu.dev>
2025-11-21 15:56:16 +0200

    jako yhden ja kahden hengen huoneisiin

--- main.cpp
+++ main.cpp
@@ -7,44 +7,52 @@ const int room_count_max = 300;
 const int price_min = 80;
 const int price_max = 100;
 
+// Kiva avustusfunktio jota käytetään tasan kerran. Varmistaa että huoneiden
+// määrä on parillinen.
+inline int make_even(int n) { return n - n % 2; }
+
 class Room {
 private:
   int price_per_night;
   bool is_booked;
   int capacity;
+  int room_number;
 
 public:
-  Room(int capacity, std::mt19937 &gen) : capacity(capacity) {
+  Room(int room_number, int capacity, std::mt19937 &gen)
+      : room_number(room_number), capacity(capacity) {
+
     std::uniform_int_distribution<> price_dist(price_min, price_max);
     price_per_night = price_dist(gen);
     is_booked = false;
   }
 
   void print_room_info() const {
-    std::cout << "Hinta yöltä: " << price_per_night
-              << ", Varattu: " << (is_booked ? "Kyllä" : "Ei") << std::endl;
+    std::cout << "Huone numero: " << room_number
+              << ", hinta yöltä: " << price_per_night
+              << ", Varattu: " << (is_booked ? "Kyllä" : "Ei")
+              << ", monenko huone: " << capacity << std::endl;
   }
 };
-
-inline int make_even(int n) { return n - n % 2; }
-
-std::vector<Room> create_rooms(std::mt19937 &gen) {
-  std::vector<Room> rooms;
-
-  std::uniform_int_distribution<> room_count_dist(room_count_min,
-                                                  room_count_max);
-  int room_count = make_even(room_count_dist(gen));
-
-  for (int i = 0; i < room_count; ++i) {
-    rooms.emplace_back(gen);
-  }
-
-  return rooms;
-}
 
 class Hotel {
 private:
   std::vector<Room> rooms;
+
+  std::vector<Room> create_rooms(std::mt19937 &gen) {
+    std::vector<Room> rooms;
+
+    std::uniform_int_distribution<> room_count_dist(room_count_min,
+                                                    room_count_max);
+    int room_count = make_even(room_count_dist(gen));
+    int halfway = room_count / 2;
+
+    for (int i = 0; i < room_count; ++i) {
+      rooms.emplace_back(i, i < halfway ? 1 : 2, gen);
+    }
+
+    return rooms;
+  }
 
 public:
   Hotel(std::mt19937 &gen) : rooms(create_rooms(gen)) {}
